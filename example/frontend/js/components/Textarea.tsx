5bab8f8f25274b616a96a7505a2fa0abdc499403
Roman Glushko <roman.glushko.m@gmail.com>
2022-06-05 15:16:18 +0300

    🎨 Styled the textarea

--- example/frontend/js/components/Textarea.tsx
+++ example/frontend/js/components/Textarea.tsx
@@ -5,28 +5,54 @@ import { currentDocument } from "../states/documents";
 import { useState } from "@hookstate/core";
 
 import "./Textarea.css"
+import textarea from "./Textarea";
 
 type TextareaProps = {
     children?: JSX.Element | JSX.Element[]
 }
 
 const Textarea = (props: TextareaProps): JSX.Element => {
+    const isSyncing = useState(true)
     const document = useState(currentDocument)
+    const docText = useState(currentDocument.text)
 
     useEvent(EventTypes.document_opened, (data) => {
-        document.set(data.document)
+        document.set(data.document);
+        docText.set(data.document.text);
     })
+
+    const handleInput = (event: React.FormEvent<HTMLTextAreaElement>) => {
+        const textarea: HTMLTextAreaElement = event.target
+        const inputEvent: InputEvent = event.nativeEvent
+
+        console.log(event)
+        console.log(`[Input] Text was ${inputEvent.inputType} "${inputEvent.data}" 
+        at ${textarea.selectionStart}:${textarea.selectionEnd}`)
+    }
+
+    function handleSelect(event: React.SyntheticEvent<HTMLTextAreaElement>) {
+        const textarea: HTMLTextAreaElement = event.target
+
+        console.log(event)
+        console.log(`Selection at ${textarea.selectionStart}:${textarea.selectionEnd}`)
+    }
 
     return <form action="">
         <h2>{ document.get().title }</h2>
-        <p className={`revision`}>rev: {document.get().revision_id}</p>
         { props.children }
+        { isSyncing.get() && <p className={`syncing-status`}>syncing 🔄</p> }
         <textarea
-            value={document.get().text}
+            className={`editbox`}
+            value={docText.get()}
             rows={30}
             cols={50}
-            autoComplete="off">
+            autoComplete="off"
+            onChange={(event) => docText.set(event.target.value)}
+            onInput={event => handleInput(event)}
+            onSelect={event => handleSelect(event)}
+        >
         </textarea>
+        <p className={`revision`}>rev: {document.get().revision_id}</p>
     </form>
 }
 
